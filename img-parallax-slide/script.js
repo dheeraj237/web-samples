aad08537c87c5f04c08b36892e25d1e80da847e1
dheeraj.suthar <dheeraj.suthar@snapdeal.com>
2022-12-16 15:53:05 +0530

    ng-sample angular app added.

--- img-parallax-slide/script.js
+++ img-parallax-slide/script.js
@@ -1,13 +1,9 @@
-
-
 const track = document.getElementById("image-track");
 
 window.onmousedown = e => {
-    console.log('down', e)
     track.dataset.mouseDownAt = e.clientX;
 }
 window.onmouseup = e => {
-    console.log('up', e);
     track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = parseFloat(track.dataset.percentage);
 }
@@ -16,19 +12,16 @@ window.onmousemove = e => {
 
     if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-    const maxDelta = window.innerWidth / 2;
-    const percentage = (mouseDelta/maxDelta) * -100;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2,
+        percentage = (mouseDelta/maxDelta) * -100;
     let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
     nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
-
     track.dataset.percentage = nextPercentage;
 
     track.animate({
         transform:`translate(${nextPercentage}%, -50%)`
     }, { duration: 4800, fill: "forwards"});
-
     for(const image of track.getElementsByClassName("image")) {
         image.animate({
             objectPosition: `${nextPercentage + 100}% center`
